6e6c88818c0d738a39ca48a8488e9b764774b592
SAI HARIKA  TADI <saiharika.tadi08@gmail.com>
2020-02-26 04:05:38 -0500

    commit on 24 Feb

--- server/index.js
+++ server/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+
+const quoteCards = require('./game/quoteCards');
+
 const app = express();
 const port = 3000;
 
 app
-    .get('/', (req, res) => res.send('This Class is Awesome!'));
+    .get('/', (req, res) => res.send('This Class is Awesome!'))
+    .get('/game/quoteCards', (req, res) => res.send(quoteCards));
+
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
