0cd38097f3cd8cbac095c1025780123aef6de1a3
SAI HARIKA  TADI <saiharika.tadi08@gmail.com>
2020-04-06 13:34:54 -0400

    Added Global Error handling

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const path = require('path');
+
 const gameController = require('./controllers/game');
-const path = require('path')
+const usersController = require('./controllers/users');
+
 
 
 const app = express();
@@ -9,15 +12,20 @@ const port = 3000;
 app
     .use(express.json())
     .use(express.urlencoded({ extended: true }))
-    .use(express.static(__dirname + '/../client/dist'))
-    .get('/', (req, res) => res.send('This Class is Awesome!'))
+    .use(express.static( __dirname + '/../client/dist'))
+    .get('/', (req, res) => res.send('This class is awesome!') )
     .use('/game', gameController)
+    .use('/users', usersController)
 
     .use((req, res) => {
-        const homePath = path.join(__dirname , '/../client/dist/index.html');
+        const homePath = path.join( __dirname , '/../client/dist/index.html');
         console.log(homePath);
         res.sendFile(homePath)
     })
-
+    .use( (err, req, res, next ) => {
+        console.error(err);
+        const errorCode = err.code || 500;
+        res.status(errorCode).send({ message: err.message });
+    } )
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
