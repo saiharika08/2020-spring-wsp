86f3a22374ca935b92d2b71ab0e2508bad9f4167
SAI HARIKA  TADI <saiharika.tadi08@gmail.com>
2020-04-30 14:08:05 -0400

    client side authorization

--- client/src/models/Users.js
+++ client/src/models/Users.js
@@ -1,16 +1,10 @@
-const Users = [
-    { Name: 'Harika', Password: '2020', Email: 'tadis1@newpaltz.edu' },
-    { Name: 'Tadi', Password: '1998', Email: 'tadis1@hawkmail.newpaltz.edu' },
-    
-];
+import myFetch from "./myFetch";
 
 export let CurrentUser = null;
 
-export function Login(email, password) {
+export async function Login(email, password) {
 
-    const user = Users.find(x => x.Email == email);
-    if(!user) throw Error('User not found');
-    if(user.Password != password) throw Error('Wrong Password');
+    const user = await myFetch('/users/login', { email, password }) ;
 
     return CurrentUser = user;
 }
