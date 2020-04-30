2cff5b81563722c7102dc3f0af960c12eb0e9872
SAI HARIKA  TADI <saiharika.tadi08@gmail.com>
2020-04-30 14:14:03 -0400

    fix bug in server side authorization

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -1,3 +1,5 @@
+/* B"H
+*/
 const express = require('express');
 
 const quoteCards = require('../models/quoteCards');
@@ -5,20 +7,35 @@ const game = require('../models/Game');
 
 const router = express.Router();
 
+router.use(function(req, res, next) {
+    if(req.userId != null ){
+        req.playerId = game.GetPlayerId(req.userId)
+    }
+    console.log({ userId: req.userId, playerId: req.playerId })
+    next();
+});
+
 router
-    .get('/', (req, res) => res.send({
-        Players: game.Players, PictureDeck: game.PictureDeck, CurrentPicture: game.CurrentPicture,
-        CardsInPlay: game.CardsInPlay.map(x=> ({...x, PlayerId: 'unknown' }) ) 
-    }) )
-    .post('/join', (req, res)=> res.send(game.Join(req.body.userId)) )
+    .get('/', (req, res) => { 
+        console.log( req.userId );
+        res.send({
+            Players: game.Players, PictureDeck: game.PictureDeck, CurrentPicture: game.CurrentPicture,
+            CardsInPlay: game.CardsInPlay.map(x=> ({...x, PlayerId: 'unknown' }) ) 
+        });
+
+    })
+
+    .post('/join', (req, res)=> res.send(game.Join(req.userId)) )
+    .post('/flipPicture', (req, res)=> res.send(game.FlipPicture()) )
+
     .get('/quoteCards', (req, res) => res.send(quoteCards) )
     .post('/quoteCards', (req, res) => {
         quoteCards.add(req.body.text);
         res.send(quoteCards.list[quoteCards.list.length - 1]);
     })
+
    .post('/cardsInPlay', (req, res) => {
-        const playerId = req.body.playerId;
-        game.SubmitCaption(req.body.caption, playerId);
+        game.SubmitCaption(req.body.caption, req.playerId);
         res.send({ success: true })
     })
 
